35a7a3d6d2d151ed29110c6b455833df09e09524
TORU URAKAWA <toru.urkw@gmail.com>
2014-05-15 05:46:15 +0900

    import

---
+++ src/ofxCocoaFloatingWindow.h
@@ -0,0 +1,15 @@
+//
+//  ofxCocoaFloatingWindow.h
+//
+//  Created by TORU URAKAWA on 2014/05/15.
+//
+//
+
+#pragma once
+
+#include "ofMain.h"
+
+class ofxCocoaFloatingWindow {
+public:
+    void setup(bool bIgnoreMouseEvents);
+};
---
+++ src/ofxCocoaFloatingWindow.mm
@@ -0,0 +1,18 @@
+//
+//  ofxCocoaFloatingWindow.mm
+//
+//  Created by TORU URAKAWA on 2014/05/15.
+//
+//
+
+#include "ofxCocoaFloatingWindow.h"
+
+void ofxCocoaFloatingWindow::setup(bool bIgnoreMouseEvents) {
+	NSOpenGLContext *context = [NSOpenGLContext currentContext];
+	NSView *view = [context view];
+	NSWindow *window = [view window];
+		
+    BOOL b = bIgnoreMouseEvents ? YES : NO;
+    [window setIgnoresMouseEvents:b];
+    [window setLevel:kCGFloatingWindowLevel];
+}
